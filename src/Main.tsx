6c7b66f84fbd126545ac9f1e87cb4400e5467e73
pietroperna24-source <pietroperna24@gmail.com>
2026-05-01 11:17:52 +0200

    Update Main.tsx

--- src/Main.tsx
+++ src/Main.tsx
@@ -1,20 +1,13 @@
-import React from 'react'; // Aggiungi questa riga!
+import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App.tsx';
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+const rootElement = document.getElementById('root');
 
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-
-// Deve esserci 'root' qui, che corrisponde all'id nel file index.html
-ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+if (rootElement) {
+  ReactDOM.createRoot(rootElement).render(
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  );
+}
