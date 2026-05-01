83fa46340b8b7a502a7894211ef38a879a3857ed
pietroperna24-source <pietroperna24@gmail.com>
2026-05-01 11:00:35 +0200

    Update Main.tsx

--- src/Main.tsx
+++ src/Main.tsx
@@ -1,7 +1,8 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App'
+import App from './App.tsx'
 
+// Deve esserci 'root' qui, che corrisponde all'id nel file index.html
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <App />
